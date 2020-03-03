bbbb1c83a0733bc732c3bb519b909d51b669db3e
matt <mxb9771@rit.edu>
2020-03-03 07:19:18 -0500

    updated to most recent changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
+const ec2_url = 'http://ec2-3-12-198-129.us-east-2.compute.amazonaws.com:8080';
+const headers = {
+    'Content-Type': 'application/json'
+}
+
 export const getProducts = () => {
     // axios.get('')
     //     .then(res => {
 
     //     });
     return [
+        // NOTE: stub for an API call made to inventory service for current products and their inventory counts
         { id: 0, name: 'Product A', price: 100, count: 5 },
         { id: 1, name: 'Product B', price: 110, count: 4 },
         { id: 2, name: 'Product C', price: 80, count: 3 },
@@ -15,28 +21,26 @@ export const getProducts = () => {
 
 export const makeOrder = (order, total, customerId) => {
     const body = {
-        customerID: customerId,
+        customerId,
         totalPrice: total,
         netProfit: total - 100,
-        itemList: order
+        // itemList: order
     }
 
-    // axios.post('', { body })
-    //     .then(res => {
-
-    //     });
+    axios.post(`${ec2_url}/order/add`, body, { headers })
+        .then(res => console.log(res))
+   
     alert('Sale Made!')
 }
 
-export const makeRefund = (orderId) => {
+export const makeRefund = async (orderId) => {
     const body = {
         orderId,
         description: 'Product Refund'
     }
 
-    // axios.post('', { body})
-    //     .then(res => {
-            
-    //     });
+    axios.post(`${ec2_url}/order/refund`, body, { headers })
+        .then(res => console.log(res))
+   
     alert('Refund Made!')
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,16 +1,18 @@
-import { LOGIN_CUSTOMER, LOGIN_SALES_REP } from './types';
+import { LOGIN_CUSTOMER, LOGIN_SALES_REP, LOGOUT } from './types';
 
 const INITIAL_STATE = {
     user_type: '',
-    uid: null
+    uid: ''
 }
 
 const Reducer = function (state = INITIAL_STATE, action) {
     switch (action.type) {
         case LOGIN_CUSTOMER:
-            return { ...state, user_type: 'customer', uid: 2 }
+            return { ...state, user_type: 'customer', uid: '77eaf7e0-d275-4a66-9386-441ad07c343e' }
         case LOGIN_SALES_REP:
-            return { ...state, user_type: 'sales_rep', uid: 1 }
+            return { ...state, user_type: 'sales_rep', uid: '77eaf7e0-d275-4a66-9386-441ad07c343e' }
+        case LOGOUT:
+            return { ...INITIAL_STATE }
         default:
             return state;
     }
